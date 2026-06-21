1c041292d8a396fe2a8835955c439371041f9c32
Abuawaish <74604342+abuawaish@users.noreply.github.com>
2026-06-22 02:26:41 +0530

    updated new version -> 1.6.9

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 with open('README.rst', encoding='utf-8') as readme_file:
     long_description = readme_file.read()
@@ -8,34 +8,48 @@ with open('CHANGELOG.txt', encoding='utf-8') as changelog_file:
 
 setup(
     name="PyAwaish",
-    version="1.6.8",
+    version="1.6.9",
     author="Abu Awaish",
     author_email="abuawaish7@gmail.com",
     description="A Python package for building dynamic MySQL-powered web applications with template support",
     long_description=long_description,
-    long_description_content_type="text/x-rst",  # Use RST for README and changelog
-    url="https://github.com/abuawaish",
-    packages=find_namespace_packages(include=["PyAwaish", "PyAwaish.*"]),
-    include_package_data=True,  # Include non-code files specified in MANIFEST.in
+    long_description_content_type="text/x-rst",
+    url="https://github.com/abuawaish/PyAwaish",  # point at the repo, not your profile
+    project_urls={
+        "Source": "https://github.com/abuawaish/PyAwaish",
+        "Bug Tracker": "https://github.com/abuawaish/PyAwaish/issues",
+    },
+    packages=find_packages(include=["PyAwaish", "PyAwaish.*"]),
+    include_package_data=True,
     package_data={
         "PyAwaish": [
-            "templates/**/*",  # Include all template files and subdirectories
-            "static/**/*"  # Include all PNG images in the static folder
+            "templates/**/*", # Include all template files and subdirectories
+            "static/**/*",  # all static assets, not just PNGs
         ],
     },
+    zip_safe=False,
     license="MIT",
-    license_files=['LICENSE.txt'],  # Explicitly include the license file
+    license_files=['LICENSE'],
     install_requires=[
-        'Flask==3.1.2',
-        'Flask-MySQLdb==2.0.0',
-        'python-dotenv==1.2.1',
-        'mysqlclient==2.2.8',
+        'Flask>=3.1.2,<4',
+        'Flask-MySQLdb>=2.0.0,<3',
+        'python-dotenv>=1.2.1,<2',
+        'mysqlclient>=2.2.8,<3',
     ],
     keywords="web application flask mysql dynamic templates",
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Framework :: Flask",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    python_requires=">=3.6",  # Specify the minimum Python version
+    python_requires=">=3.10",  # matches the real floor set by your dependencies
 )
